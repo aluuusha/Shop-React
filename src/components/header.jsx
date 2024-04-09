3b425dd3eba67d6058edc0cc46f1e8f119f16bcf
aluuusha <alikey68@mail.ru>
2024-04-09 15:13:49 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+function Header () {
+    return <nav className="#f06292 pink lighten-2">
+        <div className="container">
+            <div className="nav-wrapper">
+                <a href="#" class="brand-logo">React Shop project</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="!#">Repository</a></li>
+                </ul>
+            </div>
+        </div>
+        
+    </nav>
+}
+
+export {Header} ;
---
+++ src/components/preloader.jsx
@@ -0,0 +1,7 @@
+function Preloader() {
+    return <div className="progress #f06292 pink lighten-2">
+        <div className="indeterminate"></div>
+    </div>
+}
+
+export {Preloader};
